070d47c79d366813438934abbbb8acd18dcde5bc
songzhw <songzhw2012@gmail.com>
2015-08-30 12:10:52 +0800

    init the TabsView

---
+++ app/src/main/java/cn/song/open/util/UiUtil.java
@@ -0,0 +1,16 @@
+/**
+ * @author hzsongzhengwang
+ * @date 2014年12月12日
+ * UiUtil.java
+ * Copyright 2014 NetEase. All rights reserved. 
+ */
+package cn.song.open.util;
+
+import android.content.Context;
+import android.util.TypedValue;
+
+public class UiUtil {
+	public static int dp2px(Context ctx, int dp) {
+		return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, ctx.getResources() .getDisplayMetrics());
+	}
+}
---
+++ app/src/main/java/cn/song/open/view/tabsview/OnTabItemClickListener.java
@@ -0,0 +1,6 @@
+package cn.song.open.view.tabsview;
+
+public interface OnTabItemClickListener {
+	/**index从0开始计数*/
+	void onTabItemClick(int index);
+}
