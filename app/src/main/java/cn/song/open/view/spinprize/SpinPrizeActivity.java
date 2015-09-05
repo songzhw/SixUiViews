e1f3b6ed802de82a6745d0c01a0769cb86d99434
songzhw <songzhw2012@gmail.com>
2015-09-05 11:29:44 +0800

    add the SpinPrizeView, which is still on progress

---
+++ app/src/main/java/cn/song/open/view/spinprize/SpinPrizeActivity.java
@@ -0,0 +1,18 @@
+package cn.song.open.view.spinprize;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import cn.song.open.R;
+
+
+public class SpinPrizeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spin_prize_view);
+    }
+
+
+}
