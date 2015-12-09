a17949fe2404e5b0e3632a4f1b14ea1dddf35902
songzhw <songzhw2012@gmail.com>
2015-12-09 10:57:48 +0800

    Bounced Scroll View

---
+++ app/src/main/java/cn/six/open/others/bounceview/BounceSvActivity.java
@@ -0,0 +1,19 @@
+package cn.six.open.others.bounceview;
+
+import android.os.Bundle;
+import android.app.Activity;  
+import android.view.Window;
+
+import cn.six.open.R;
+
+public class BounceSvActivity extends Activity {
+  
+    @Override  
+    protected void onCreate(Bundle savedInstanceState) {  
+        super.onCreate(savedInstanceState);  
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  
+        setContentView(R.layout.activity_bounce_sv);
+    }  
+
+  
+} 
