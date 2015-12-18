b9a844bf8093bd0050fc5ab441eec254961d934b
songzhw <songzhw2012@gmail.com>
2015-12-18 11:13:40 +0800

    custom EditText for input the bank card number

---
+++ app/src/main/java/cn/six/open/others/edittext/BankNumActivity.java
@@ -0,0 +1,22 @@
+package cn.six.open.others.edittext;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.InputType;
+import android.widget.EditText;
+
+public class BankNumActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        EditText et = new BankCardNumEt(this);
+//        et.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL
+//            | InputType.TYPE_NUMBER_FLAG_SIGNED);
+
+        EditText et = new EditText(this);
+        et.setInputType(InputType.TYPE_CLASS_PHONE);
+        setContentView(et);
+    }
+}
