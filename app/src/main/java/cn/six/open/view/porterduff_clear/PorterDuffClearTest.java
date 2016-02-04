af8ae66c0155759f5a94d7a6e0c7a715a42e4d7d
songzhw <songzhw2012@gmail.com>
2016-02-04 15:43:20 +0800

    Learn PorterDuff.Mode.CLEAR again!

---
+++ app/src/main/java/cn/six/open/view/porterduff_clear/PorterDuffClearTest.java
@@ -0,0 +1,28 @@
+package cn.six.open.view.porterduff_clear;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.view.View;
+
+import cn.six.open.R;
+
+/**
+ * Created by songzhw on 2016/2/4
+ */
+public class PorterDuffClearTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_porterduff_clear);
+
+    }
+
+
+}
