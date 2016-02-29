df14941559ca4c40c0bfcf029e7ff592aa718f6b
songzhw <songzhw2012@gmail.com>
2016-02-29 15:44:55 +0800

    learn https://github.com/cooltechworks/CreditCardView

---
+++ app/src/main/java/others/credit_card_cool/core/pager/IActionListener.java
@@ -0,0 +1,7 @@
+package others.credit_card_cool.core.pager;
+
+public interface IActionListener {
+        public void onActionComplete(CreditCardFragment fragment);
+        public void onEdit(CreditCardFragment fragment, String edit);
+
+    }
