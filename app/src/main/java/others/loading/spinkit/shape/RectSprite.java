6b34b20309c7d4d038be56fd9ded5615d0ac7f30
songzhw <songzhw2012@gmail.com>
2016-04-13 18:03:16 +0800

    learn SpinKit

--- app/src/main/java/others/loading/spinkit/shape/RectSprite.java
+++ app/src/main/java/others/loading/spinkit/shape/RectSprite.java
@@ -3,6 +3,7 @@ package others.loading.spinkit.shape;
 import android.animation.ValueAnimator;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Rect;
 
 public class RectSprite extends ShapeSprite {
     @Override
@@ -13,7 +14,14 @@ public class RectSprite extends ShapeSprite {
     @Override
     public void drawShape(Canvas canvas, Paint paint) {
         if (getDrawBounds() != null) {
-            canvas.drawRect(getDrawBounds(), paint);
+            Rect rect = getDrawBounds();
+            rect = new Rect(
+                    rect.left + rect.width() / 3,
+                    rect.top + rect.height() / 3,
+                    rect.right,
+                    rect.bottom
+            );
+            canvas.drawRect(rect, paint);
         }
     }
 }
