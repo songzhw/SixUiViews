a23d3c3614299dc99ca8922ffb62791e41e293fe
songzhw <songzhw2012@gmail.com>
2017-02-21 20:31:53 -0500

    OneBindingAdapter for Rv

---
+++ app/src/main/java/cn/six/open/view/rv/OneBindingAdapter/OneBindingViewHolder.java
@@ -0,0 +1,11 @@
+package cn.six.open.view.rv.OneBindingAdapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class OneBindingViewHolder extends RecyclerView.ViewHolder {
+
+    public OneBindingViewHolder(View itemView) {
+        super(itemView);
+    }
+}
