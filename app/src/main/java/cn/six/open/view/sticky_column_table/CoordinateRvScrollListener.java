1175e2e7e4eb08b9b07d0550ede8c85fdcf5ab47
songzhw <songzhw2012@gmail.com>
2017-09-19 19:13:39 -0400

    remove unnecessary base class

--- app/src/main/java/cn/six/open/view/sticky_column_table/CoordinateRvScrollListener.java
+++ app/src/main/java/cn/six/open/view/sticky_column_table/CoordinateRvScrollListener.java
@@ -2,11 +2,19 @@ package cn.six.open.view.sticky_column_table;
 
 import android.support.v7.widget.RecyclerView;
 
-public class CoordinateRvScrollListener extends MultiRvScrollListener {
+public class CoordinateRvScrollListener extends RecyclerView.OnScrollListener {
     private RecyclerView rvOther;
 
     public CoordinateRvScrollListener(RecyclerView rvOther) {
         this.rvOther = rvOther;
+    }
+
+    @Override
+    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+        super.onScrollStateChanged(recyclerView, newState);
+        if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+            recyclerView.removeOnScrollListener(this);
+        }
     }
 
     @Override
