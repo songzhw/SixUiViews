d430351473efda43dc2b9c6c7d8df2ce31997b73
songzhw <songzhw2012@gmail.com>
2018-03-03 20:43:09 -0500

    create ExpandableLayout and demo

---
+++ app/src/main/java/cn/six/open/view/expandable/ExpandableLayout.java
@@ -0,0 +1,20 @@
+package cn.six.open.view.expandable;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+
+public class ExpandableLayout extends LinearLayout {
+    public ExpandableLayout(Context context) {
+        super(context);
+    }
+
+    public ExpandableLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+
+}
