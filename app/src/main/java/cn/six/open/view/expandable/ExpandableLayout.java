22d02635feb540e6866b045bfca5ea47272dbda2
songzhw <songzhw2012@gmail.com>
2018-03-05 20:10:02 -0500

    ExpandableLayout : get children view

--- app/src/main/java/cn/six/open/view/expandable/ExpandableLayout.java
+++ app/src/main/java/cn/six/open/view/expandable/ExpandableLayout.java
@@ -3,10 +3,13 @@ package cn.six.open.view.expandable;
 import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.LinearLayout;
 
 
 public class ExpandableLayout extends LinearLayout {
+    private View headerView, contentView;
+
     public ExpandableLayout(Context context) {
         super(context);
         init(context);
@@ -21,5 +24,14 @@ public class ExpandableLayout extends LinearLayout {
         setOrientation(VERTICAL);
     }
 
+    //  onFinishInflate -> onMeasure -> onSizeChanged -> onLayout
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+    }
 
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
 }
