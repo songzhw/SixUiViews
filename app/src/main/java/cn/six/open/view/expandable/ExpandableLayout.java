09906300d030a750ea2721958fee1154d954efa6
songzhw <songzhw2012@gmail.com>
2018-03-05 20:17:53 -0500

    ExpandableLayout : get contentHeight

--- app/src/main/java/cn/six/open/view/expandable/ExpandableLayout.java
+++ app/src/main/java/cn/six/open/view/expandable/ExpandableLayout.java
@@ -8,7 +8,10 @@ import android.widget.LinearLayout;
 
 
 public class ExpandableLayout extends LinearLayout {
+
     private View headerView, contentView;
+
+    private int contentHeight;
 
     public ExpandableLayout(Context context) {
         super(context);
@@ -24,15 +27,26 @@ public class ExpandableLayout extends LinearLayout {
         setOrientation(VERTICAL);
     }
 
-    // ★★★  onFinishInflate -> onMeasure -> onSizeChanged -> onLayout  ★★★
+    // ★★★  onFinishInflate -> onAttachedToWindow -> onMeasure -> onSizeChanged -> onLayout  ★★★
+
+
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
-        if(getChildCount() != 2){
+        if (getChildCount() != 2) {
             throw new RuntimeException("ExpandableLayout could only have two children: header and content!");
         }
         headerView = getChildAt(0);
         contentView = getChildAt(1);
+
+
+        contentHeight = contentView.getMeasuredHeight(); //此时contentView.getHeight()仍是0
+
+        contentView.setVisibility(View.GONE);
+        headerView.setOnClickListener(v -> {
+            System.out.println("szw click header");
+        });
+        System.out.println("szw onSizeChanged contentHeight = "+contentHeight);
     }
 }
