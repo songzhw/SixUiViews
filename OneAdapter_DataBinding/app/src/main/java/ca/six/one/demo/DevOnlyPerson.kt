42f67c75c7cd5231fd3b37e9c41240065a190e65
songzhw <songzhw2012@gmail.com>
2018-09-13 21:41:21 -0400

    Demo

---
+++ OneAdapter_DataBinding/app/src/main/java/ca/six/one/demo/DevOnlyPerson.kt
@@ -0,0 +1,4 @@
+package ca.six.one.demo
+
+data class DevOnlyPerson(var id : Long, var name: String, var isMale : Boolean) {
+}
---
+++ OneAdapter_DataBinding/app/src/main/java/ca/six/one/demo/DevOnlyUser.kt
@@ -0,0 +1,4 @@
+package ca.six.one.demo
+
+class DevOnlyUser(var name : String, var age : Int) {
+}
