41c3ccccae71f964123ec2d23071b8634987fdec
songzhw <songzhw2012@gmail.com>
2018-09-13 20:57:15 -0400

    refactor

--- app/src/main/java/cn/six/open/view/sticky_column_table/IStickyColumnTableInflater.java
+++
@@ -1,8 +0,0 @@
-package cn.six.open.view.sticky_column_table;
-
-import cn.six.open.view.rv.OneAdapter.RvViewHolder;
-
-public interface IStickyColumnTableInflater<T> {
-    void bindLeft(RvViewHolder vh, T s, int position);
-    void bindRight(RvViewHolder vh, T s, int position);
-}
