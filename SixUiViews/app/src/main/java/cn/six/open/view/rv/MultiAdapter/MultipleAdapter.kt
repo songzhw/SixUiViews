b4ae7325aae2cab223e2fd87bab254d29c8f8884
songzhw <songzhw2012@gmail.com>
2019-12-03 16:27:46 -0500

    reafctor the MultipleAdapter (removed for loop)

--- SixUiViews/app/src/main/java/cn/six/open/view/rv/MultiAdapter/MultipleAdapter.kt
+++ SixUiViews/app/src/main/java/cn/six/open/view/rv/MultiAdapter/MultipleAdapter.kt
@@ -4,50 +4,37 @@ import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import cn.six.open.view.rv.OneAdapter.RvViewHolder
 
-/* TODO
-    (to refactor) we may have 100 data, and only 2 IRvTypes, so it's impossible to make `data: List<IRvType>`
-    also, I don't want to do a for iteration in the getItemViewType() and other methods
- */
-class MultipleAdapter<T>(val data: List<T>, val types: List<IRvType<T>>) : RecyclerView.Adapter<RvViewHolder>() {
+class MultipleAdapter(val data: List<Any>, val types: Map<Class<Any>, IRvType>) : RecyclerView.Adapter<RvViewHolder>() {
+    override fun getItemViewType(position: Int): Int {
+        val datum = data.get(position)
+        val clazz = datum::class.java
+        return types.get(clazz)?.getLayoutResId()!!
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
+        // viewType即layoutResId
+        return RvViewHolder.createViewHolder(parent, viewType)
+    }
+
+
+    override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
+        if (data.size > position) {
+            val datum = data.get(position)
+            val clazz = datum::class.java
+            val rvType: IRvType? = types.get(clazz)
+            rvType?.render(holder, datum, position)
+        }
+    }
+
 
     override fun getItemCount(): Int {
         return data.size
     }
 
-    override fun getItemViewType(position: Int): Int {
-        for (rvType in types) {
-            val viewType = rvType.getType(position)
-            if (viewType != IRvType.TYPE_INVALID) {
-                return viewType
-            }
-        }
-        return super.getItemViewType(position)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
-//        for (rvType in types) {
-//            val viewType = rvType.getType(position)
-//            if (viewType != IRvType.TYPE_INVALID) {
-//                return rvType.get
-//            }
-//        }
-        TODO("not implemented")
-    }
-
-
-    override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
 }
 
 
-interface IRvType<T> {
-    fun getType(position: Int): Int
-    fun getLayoutResId(viewType: Int): Int
-    fun apply(vh: RvViewHolder, datum: T, position: Int)
-
-    companion object {
-        val TYPE_INVALID = -99
-    }
+interface IRvType {
+    fun getLayoutResId(): Int
+    fun render(vh: RvViewHolder, datum: Any, position: Int)
 }
