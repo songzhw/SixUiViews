47bc93cc9bf114f348fb85a15c95ac467f478ee0
songzhw <songzhw2012@gmail.com>
2022-01-30 00:04:18 -0500

    chore: refactor the whole structure

---
+++ SixUiViews/app/src/main/java/ca/six/demo/sixuiviews/MainActivity.kt
@@ -0,0 +1,11 @@
+package ca.six.demo.sixuiviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
