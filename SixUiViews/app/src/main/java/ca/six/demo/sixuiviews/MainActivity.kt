f47974f29231c312263e5a6d37629ecba813f384
songzhw <songzhw2012@gmail.com>
2022-01-30 01:14:03 -0500

    update the main actv

--- SixUiViews/app/src/main/java/ca/six/demo/sixuiviews/MainActivity.kt
+++ SixUiViews/app/src/main/java/ca/six/demo/sixuiviews/MainActivity.kt
@@ -1,12 +1,24 @@
 package ca.six.demo.sixuiviews
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn1.setOnClickListener { nav<ColorMaskActivity>() }
     }
+
+}
+
+
+inline fun <reified T>  Activity.nav() {
+    val intent2 = Intent(this, T::class.java)
+    startActivity(intent2)
 }
